9aabf75414238d259eeff6c43e94b8653988127b
tf-vishal <vvishalsharma0609@gmail.com>
2026-02-15 19:57:06 +0530

    DAY 29,30,31 commits

---
+++ Day29/Readme.jsx
@@ -0,0 +1,11 @@
+useEffect(() => {
+  // This runs after every render
+});
+
+useEffect(() => {
+  // This runs only on mount (when the component appears)
+}, []);
+
+useEffect(() => {
+  // This runs on mount *and also* if either a or b have changed since the last render
+}, [a, b]);
---
+++ Day31/react-env/src/DefaultProfile.jsx
@@ -0,0 +1,5 @@
+const DefaultProfile = () => {
+    return <p>Oh, Nothing to see here</p>;
+};
+
+export default DefaultProfile;
